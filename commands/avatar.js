ceb41a04c907976a9c63c337fe0b83450cde7f03
Eliam Rodríguez <elax27@hotmail.com>
2020-10-09 00:03:26 -0400

    Fixed avatar not sending
    With the update to discord.js v12 the avatar was not sending so I changed so it sends an embed with the image and that you can mention a user to view it's avatar.

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,34 +1,30 @@
+const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-
+  let user = message.author;
+  let avatarEmbed = new MessageEmbed();
   let msg = await message.channel.send("Generando avatar...");
   if (args[0]) {
-    const user = message.mentions.members.first().user;
+    user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
     if (!user) {
-      msg.delete();
+      await msg.delete();
       return message.reply("Please mention a valid user!");
     }
-    await message.channel.send({files: [
-      {
-        attachment: user.displayAvatarURL,
-        name: "avatar.png"
-      }
-    ]});
-    msg.delete();
-    return;
   }
-  await message.channel.send({files: [
-    {
-      attachment: message.author.displayAvatarURL,
-      name: "avatar.png"
-    }
-  ]});
+  let avatarUrl = await user.displayAvatarURL({ format:'png', dynamic: true, size: 512});
+  avatarEmbed.setTitle(`${user.tag}'s Avatar`);
+  avatarEmbed.setDescription(`[Avatar Url](${avatarUrl})`);
+  avatarEmbed.setImage(avatarUrl);
+  avatarEmbed.setColor(16035249);
 
-    msg.delete();
+  await msg.delete();
+  return message.channel.send(avatarEmbed);
 };
 
 module.exports.config = {
   name: "avatar",
-  usage: "",
+  description: "Sends the user avatar",
+  usage: "avatar [user]",
+  category: "Utility",
   level: 3
 };
